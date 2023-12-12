79ba2559c8a2765df0f81ff5ce86860674a431d0
Virgile <virgile.testut@gmail.com>
2023-12-12 17:27:24 +0100

    cleaning the repo

---
+++ z02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtestut <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 11:33:36 by vtestut           #+#    #+#             */
+/*   Updated: 2022/07/13 11:40:04 by vtestut          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_put_hexa(int c, int b)
+{
+	char	*base16;
+
+	base16 = "0123456789abcdef";
+	if (c >= 16)
+	{
+		ft_put_hexa(c / 16);
+		ft_put_hexa(c % 16);
+	}
+	else
+	{
+		if (b == 0)
+			ft_putchar('0');
+		ft_putchar(base[c]);
+	}
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] < 32 || str[i] == 127)
+		{
+			ft_putchar('\\');
+			j = str[i];
+			ft_put_hexa(j, 0);
+		}
+		else
+		{
+			ft_putchar(str[i]);
+		}
+	i++;
+	}
+}
---
+++ z06/ex00/ft_print_program_name.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtestut <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/18 18:55:52 by vtestut           #+#    #+#             */
+/*   Updated: 2022/07/18 18:59:04 by vtestut          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	ft_putstr(argv[0]);
+	ft_putchar('\n');
+	return (0);
+}
